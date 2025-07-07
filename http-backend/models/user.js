9030468cbe80883da38e98c09a7d64589b531c44
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-08 01:19:24 +0530

    task schema

--- http-backend/models/user.js
+++ http-backend/models/user.js
@@ -5,4 +5,22 @@ const  userprofileSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    status: {
+      type: String,
+      enum: ['Todo', 'In Progress', 'Done'],
+      default: 'Todo'
+    },
+    priority: {
+      type: String,
+      enum: ['Low', 'Medium', 'High'],
+      default: 'Medium'
+    }
+  }, { timestamps: true });
+
+module.exports
 module.exports = mongoose.model('UserProfile', userprofileSchema);
