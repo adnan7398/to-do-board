1b004bd6efc1d5b51dda8a70c3311415ac4d8ab6
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-08 00:10:06 +0530

    usermodel

---
+++ http-backend/models/user.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const  userprofileSchema = new mongoose.Schema({
+    firstName: { type: String, required: true, minlength: 2, maxlength: 100 },
+    lastName: { type: String, required: true, minlength: 2, maxlength: 20 },
+    email: { type: String, required: true, unique: true },
+})
