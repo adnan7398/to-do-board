d0642226b3462aef026c627bc1641da261207d00
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-08 00:00:43 +0530

    addeD

---
+++ http-backend/server.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use("/user", adminRoutes);
+const PORT = process.env.PORT || 3000;
+console.log(`MongoDB URL : ${process.env.MONGO_URL}`);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
